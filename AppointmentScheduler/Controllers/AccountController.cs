a8dab1af27226ee529a8450eaf99f2f063711488
DUNES <DUNES@MAPPOINT>
2024-08-08 09:01:19 +0100

    Add project files.

---
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using AppointmentScheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            db = _db;
+
+        }
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ AppointmentScheduler/Utility/Helper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AppointmentScheduler.Utility
+{
+    public static class Helper
+    {
+
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Admin,Text=Helper.Admin}
+                };
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem{Value=Helper.Patient,Text=Helper.Patient},
+                    new SelectListItem{Value=Helper.Doctor,Text=Helper.Doctor}
+                };
+            }
+        }
+
+    }
+}
